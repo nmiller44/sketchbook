2ef87610a8a824bf64cdfefd04f7c0403a6a7d4e
nmiller44 <github@nm-projects.com>
2015-09-30 15:29:15 -0500

    json

--- R/json.R
+++ R/json.R
@@ -1,12 +1,12 @@
 givejson <- function(url="") {
   data <- fromJSON(url)
   
-  #datakeepers <- c("Model","Price","MPG.highway","Horsepower","Passengers","Weight")
-  datakeepers <- c("Model","Price","Horsepower")
+  datakeepers <- c("Model","Price","MPG.highway","Horsepower","Passengers","Weight")
+  #datakeepers <- c("Model","Price","Horsepower")
   
   newdata <- data[datakeepers]
   
   list(
-    json = toJSON(data)
+    json = toJSON(newdata)
   )
 }
